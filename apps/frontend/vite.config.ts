f6000f5367a6ce23c8d86cd1f8857b68a8c2a5b3
hibohiboo <hibohiboo66+github@gmail.com>
2025-10-25 18:15:59 +0900

    fix 不要なrollup 削除

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -26,13 +26,6 @@ export default defineConfig({
   },
   worker: {
     format: 'es',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
   },
 
   build: {
