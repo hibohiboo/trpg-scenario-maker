47e1041cdb48fd3efca2db14d05949a81f229f79
hibohiboo <hibohiboo66+github@gmail.com>
2025-10-29 21:50:05 +0900

    fix config #25

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,8 +1,36 @@
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    projects: [
+      {
+        plugins: [
+          storybookTest({
+            // https://storybook.js.org/docs/writing-tests/integrations/vitest-addon
+
+            tags: {
+              include: ['test'],
+              exclude: ['experimental'],
+            },
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          // Enable browser mode
+          browser: {
+            enabled: true,
+            // Make sure to install Playwright
+            provider: playwright({}),
+            headless: true,
+            instances: [{ browser: 'chromium' }],
+          },
+          setupFiles: ['./.storybook/vitest.setup.ts'],
+        },
+      },
+    ],
   },
 });
