50c9c78ab6ed346e8ff17fdf8aacd42a5946fa4f
hibohiboo <hibohiboo66+github@gmail.com>
2025-11-07 21:36:08 +0900

    add unused imports #38

--- packages/eslint-config-custom/defaults.js
+++ packages/eslint-config-custom/defaults.js
@@ -4,6 +4,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import sonarjs from 'eslint-plugin-sonarjs';
+import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
 
 const filename = fileURLToPath(import.meta.url);
@@ -27,7 +28,22 @@ export default defineConfig([
       sonarjs.configs.recommended,
       ...eslintImport,
     ],
+    plugins: { 'unused-imports': unusedImports },
     rules: {
+      // unuserd-importsのrecommended設定を適用
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      // ここまで unuserd-importsのrecommended設定を適用
       'import/order': [
         'warn',
         {
@@ -98,55 +114,5 @@ export default defineConfig([
 //     // The typescript-eslint FAQ provides guidance here:
 //     // https://typescript-eslint.io/troubleshooting/faqs/general/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 //     'no-undef': 'off',
-//     // unuserd-importsのrecommended設定を適用
-//     'no-unused-vars': 'off',
-//     '@typescript-eslint/no-unused-vars': 'off',
-//     'unused-imports/no-unused-imports': 'warn',
-//     'unused-imports/no-unused-vars': [
-//       'warn',
-//       {
-//         vars: 'all',
-//         varsIgnorePattern: '^_',
-//         args: 'after-used',
-//         argsIgnorePattern: '^_',
-//       },
-//     ],
-//     // ここまで unuserd-importsのrecommended設定を適用
-//     'import/order': [
-//       'warn',
-//       {
-//         groups: [
-//           'builtin',
-//           'external',
-//           'internal',
-//           'parent',
-//           'sibling',
-//           'index',
-//           'object',
-//           'type',
-//         ], // importの並び順の設定
-//         pathGroupsExcludedImportTypes: ['builtin'],
-//         pathGroups: [
-//           { pattern: '@src/**', group: 'parent', position: 'before' },
-//         ], // エイリアスの位置を指定
-//         alphabetize: { order: 'asc' }, // グループ内のソート順
-//       },
-//     ],
-//     'import/prefer-default-export': ['off'],
-//     'import/no-unresolved': [
-//       'error',
-//       {
-//         ignore: ['^hono/.+'],
-//       },
-//     ],
-//     'import/extensions': ['off'],
-//   },
-//   settings: {
-//     'import/resolver': {
-//       node: {
-//         extensions: ['.js', '.jsx', '.ts', '.tsx'],
-//       },
-//       typescript: {},
-//     },
-//   },
+
 // });
