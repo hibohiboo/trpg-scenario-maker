f1faae898a4f5aa2b94467c7969a1359d1d5a2f5
hibohiboo <hibohiboo66+github@gmail.com>
2025-11-10 00:25:38 +0900

    ad sample image #43

--- packages/rdb/migrations/0001_spicy_venus.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "images" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"data_url" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp NOT NULL
-);
